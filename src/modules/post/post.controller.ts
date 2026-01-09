aaa8424a9e905de7f1c2d36625947ecd12e8d7d8
tasniaarhamarham <tasniaarhamarham@gmail.com>
2026-01-10 04:03:43 +0600

    without tags everything fixed

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,44 +1,66 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
-import { Post } from "../../../generated/prisma/client";
-import { success } from "better-auth/*";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import { boolean } from "better-auth/*";
 
-const createPost=async(req:Request,res:Response)=>{
-    try{
-    const result=await postService.createPost(req.body as Omit<Post,"id"|"createdAt"|"updatedAt">,req?.user?.id as string);
-   console.log("result",result);
-    if(result){
-        res.status(200).json({success:true,message:"post created successfully",data:JSON.stringify(result)});
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const result = await postService.createPost(
+      req.body as Omit<Post, "id" | "createdAt" | "updatedAt">,
+      req?.user?.id as string
+    );
+    console.log("result", result);
+    if (result) {
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: "post created successfully",
+          data: JSON.stringify(result),
+        });
     }
-}
-catch(error:any){
-    res.status(400).json({success:false,message:error.message})
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-}
-}
+const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const { search, tags, isFeatured, status, authId } = req?.query;
 
-
-const getAllPost=async(req:Request,res:Response)=>{
-    try{
-      const {search}= req?.query;
-      const searchString=typeof search === "string"?search:undefined;
-      const result=await postService.getAllPost({search:searchString });
-      res.status(200).json({
-        success:true,
-        data:result
-      })
-    }
-    catch(error:any){
-        res.status(400).json({success:false,message:error.message})
+    const searchString = typeof search === "string" ? search : undefined;
+    const tagsArray = (typeof tags === "string" && tags?.split(",")) || [];
+    console.log("isFeatured",typeof isFeatured);
     
-    }
-}
+    const postFeatured=isFeatured as string|any;
+    const featured =
+    postFeatured === "true" || postFeatured === true
+      ? true
+      : postFeatured === "false" || postFeatured === false
+      ? false
+      : undefined;
+  
 
+    const statusPost = status as PostStatus;
+    const Id = authId as string;
 
+    const result = await postService.getAllPost({
+      search: searchString,
+      tags: tagsArray,
+      isFeatured: featured,
+      status: statusPost,
+      authId: Id,
+    });
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-
-
-
-export const postController={
-    createPost,getAllPost
-}
+export const postController = {
+  createPost,
+  getAllPost,
+};
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,38 +1,72 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import {
+  PostWhereInput,
+  PostWhereUniqueInput,
+} from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
-const createPost=async(Post:Omit<Post,"id"|"createdAt"|"updatedAt">,id:string)=>{
-    const result=await prisma.post.create({data:{...Post,authId:id}});
-    return result;
-}
-const getAllPost=async(payload?:{search?:string|undefined})=>{
-    const result=await prisma.post.findMany({
-        where: 
+const createPost = async (
+  Post: Omit<Post, "id" | "createdAt" | "updatedAt">,
+  id: string
+) => {
+  const result = await prisma.post.create({ data: { ...Post, authId: id } });
+  return result;
+};
+const getAllPost = async ({
+  search,
+  tags,
+  isFeatured,
+  status,
+  authId,
+}: {
+  search: string | undefined;
+  tags: string[] | [];
+  isFeatured: boolean | undefined;
+  status: PostStatus | undefined;
+  authId: string | undefined;
+}) => {
+  const anyConditions: PostWhereInput[] = [];
+  if (search) {
+    anyConditions.push({
+      OR: [
         {
-            OR:[
-                {
-                    title:{
-                        contains:payload?.search as string,
-                        mode:'insensitive'
-                    }
-                },
-                {
-                    content:{
-                        contains:payload?.search as string,
-                        mode:'insensitive'
-                    }
-                },
-                {
-                    tags:{
-                        has:payload?.search as string
-                    }
-                }
-            ]
-        } 
+          title: {
+            contains: search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+          content: {
+            contains: search as string,
+            mode: "insensitive",
+          },
+        },
+      ],
     });
-    return result;
-}
+  }
+  if (tags && tags?.length > 0) {
+    anyConditions.push({
+      tags: {
+        has: search as string,
+      },
+    });
+  }
+  if (typeof isFeatured==="boolean") {
+    anyConditions.push({ isFeatured });
+  }
+  if(status){
+    anyConditions.push({status})
+  }
+  if(authId){
+    anyConditions.push({authId})
+  }
+  const result = await prisma.post.findMany({
+    where:  { AND: anyConditions },
+  });
+  return result;
+};
 
-export const postService={
-    createPost,getAllPost
-}
+export const postService = {
+  createPost,
+  getAllPost,
+};
